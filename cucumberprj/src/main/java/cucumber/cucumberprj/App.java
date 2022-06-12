0228ccf15f321f75923fddb9e2e8ff9b64c9f804
yoganand_bheema <yoganand_bheema@yoganand>
2022-06-12 22:34:49 +0530

    First my cucucmber commit

---
+++ cucumberprj/src/main/java/cucumber/cucumberprj/App.java
@@ -0,0 +1,13 @@
+package cucumber.cucumberprj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
